50761ecfaef3fe587b63c4527a1965c919f28b39
rosemm <hartmanr1@chop.edu>
2023-05-10 10:46:32 -0400

    Use env variabable for API key

--- scripts/make_pathways_repeating.R
+++ scripts/make_pathways_repeating.R
@@ -2,11 +2,11 @@
 # then makes any instruments with "_pathway" in their name repeating
 # and imports that info back to the project via the API again
 
-source(here::here("secrets", "secrets.R")) # get the API token
+# API tokens stored in .Renviron https://cran.r-project.org/web/packages/httr/vignettes/secrets.html#environment-variables
 
 # get the current list of all instruments
 url <- "https://redcap.chop.edu/api/"
-formData <- list("token"=token_NALMS_testing,
+formData <- list("token"=Sys.getenv("token_NALMS_testing"),
                  content='instrument',
                  format='csv',
                  returnFormat='json'
@@ -21,7 +21,7 @@ pathways <- grep(x = instruments, pattern = "_pathway", value = TRUE)
 repeating_instruments <- paste(c('form_name, custom_form_label', paste0('"', pathways, '", NA')), collapse ="\n")
   
 # import the list of repeating instruments via redcap API
-formData <- list("token"=token_NALMS_testing,
+formData <- list("token"=Sys.getenv("token_NALMS_testing"),
                  content='repeatingFormsEvents',
                  format='csv',
                  data=repeating_instruments,
