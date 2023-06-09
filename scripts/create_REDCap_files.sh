a260e81c431f29ab8c77076099aa1d8af4496c3c
rosemm <hartmanr1@chop.edu>
2023-06-09 15:23:35 -0400

    Add script to generate the alert to nudge inactive participants

--- scripts/create_REDCap_files.sh
+++ scripts/create_REDCap_files.sh
@@ -22,15 +22,17 @@ zip -j pathways/$pathway/$pathway.zip pathways/$pathway/instrument.csv templates
 ### individually generated survey_settings files for each pathway. This will allow us to have different text
 ### for each pathway, but for the moment they are all getting the same generic text.
 
-### We also need to create two csv files to upload, one with the automated survey invitation, and the other with the alert they get when the pathway is marked as complete.
+### We also need to create three csv files to upload, one with the automated survey invitation, and the others with the alerts they get when the pathway is marked as complete or if they're inactive for two weeks.
 
 bash scripts/create_asi.sh $pathway
-bash scripts/create_completed_alert.sh $pathway 
+bash scripts/create_completed_alert.sh $pathway
+bash scripts/create_nudge_alert.sh $pathway 
 
 ### Lastly, let's delete some of the helper files we created in this process
 
 rm pathways/$pathway/asi.md ## This was a helper file with the html text of the asi
 rm pathways/$pathway/completed_alert.md  ## this contained the html text of the completed alert
+rm pathways/$pathway/nudge_alert.md  ## this contained the html text of the nudge alert
 rm pathways/$pathway/instrument.csv ## this is now inside the zipped file
 rm pathways/$pathway/survey_settings.csv ## this is now inside the zipped file
 rm pathways/$pathway/list_of_modules ## helper file 
---
+++ scripts/create_nudge_alert.sh
@@ -0,0 +1,33 @@
+### This script creates the "nudge" email text to send participants who have been inactive for at least two weeks
+### It takes a single arguement, the pathway color, and returns
+### html that can be inserted into the REDCap ASI for that pathway.
+
+pathway=$1
+
+alert_location=pathways/$pathway/nudge_alert.md
+alert_csv=pathways/$pathway/nudge_alert.csv 
+
+### Note that the [pathway] is a field in REDCap, not the directory in this repo
+### although they should match because one of the conditions of sending the email
+### will have to be [pathway]="$pathway"
+
+## body of the email:
+
+echo "<p>Hi!</p>
+<p>We noticed that it has been a while since you logged any progress in your DART Program learning pathway:</p>
+<p>[survey-link:"$pathway"_pathway][new-instance]</p>
+<p>This link will take you to a page with the full list of education modules curated for you by our team. Indicate which ones you've been working on, and click "Submit" at the bottom of the form to log your progress. You can update your progress as many times as you want, either by following the link in this email, or by pasting the following url into your browser:</p>
+<p>[survey-url:"$pathway"_pathway][new-instance] This link is unique to you and should not be forwarded to others.</p>
+<p>Log in today to try out the next module in your learning pathway!</p>
+<p>We look forward to seeing you on the platform, and please don't hesitate to email us if you have any technical (or other) questions.</p>
+<p>Sincerely,</p>
+<p>The DART Team at Children's Hospital of Philadelphia</p>
+<p>P.S. If you have decided that participating in the DART program isn't for you after all, reply to this email to let us know and we will unenroll you from the study.</p>" > $alert_location 
+
+### The text from alert_location will then be piped into a csv with all of the survey settings.
+
+head -1 templates/alerts.csv > $alert_csv
+
+echo ',Inactive nudge '$pathway' pathway,LOGIC,,ANY,[pathway] = "'$pathway'" and datediff([survey-date-completed:test_color_pathway][last-instance], "today", "d", "ymd") > 14 AND [stop_emails] <> "1" AND [complete] <> "2", "today", "d", "ymd") > 14 AND [stop_emails] <> "1" AND [complete] <> "2",Y,RECORD,DATE,,,,,,,,8/21/2023  9:00:00 AM,SCHEDULE,,14,DAYS,,12/1/2023  9:00:00 AM,EMAIL,,dart@chop.edu,[survey-participant-email],,,,Log back in and keep learning!,"'$(cat pathways/$pathway/nudge_alert.md)'",,{},{},Y,,,N' >>$alert_csv
+
+
