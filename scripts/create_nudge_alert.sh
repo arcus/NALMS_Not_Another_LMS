77b15a05e429132e2b4c3a328c2785c49c24474e
rosemm <hartmanr1@chop.edu>
2023-06-10 17:09:21 -0400

    Consolidate the two alerts into one csv

--- scripts/create_nudge_alert.sh
+++ scripts/create_nudge_alert.sh
@@ -5,7 +5,7 @@
 pathway=$1
 
 alert_location=pathways/$pathway/nudge_alert.md
-alert_csv=pathways/$pathway/nudge_alert.csv 
+alert_csv=pathways/$pathway/alerts.csv 
 
 ### Note that the [pathway] is a field in REDCap, not the directory in this repo
 ### although they should match because one of the conditions of sending the email
@@ -25,9 +25,8 @@ echo "<p>Hi!</p>
 <p>P.S. If you have decided that participating in the DART program isn't for you after all, reply to this email to let us know and we will unenroll you from the study.</p>" > $alert_location 
 
 ### The text from alert_location will then be piped into a csv with all of the survey settings.
+### NOTE: This script adds a line to the existing alerts.csv file, and therefore must be run AFTER create_completed_alert.sh
 
-head -1 templates/alerts.csv > $alert_csv
-
-echo ',Inactive nudge '$pathway' pathway,LOGIC,,ANY,[pathway] = "'$pathway'" and datediff([survey-date-completed:test_color_pathway][last-instance], "today", "d", "ymd") > 14 AND [stop_emails] <> "1" AND [complete] <> "2", "today", "d", "ymd") > 14 AND [stop_emails] <> "1" AND [complete] <> "2",Y,RECORD,DATE,,,,,,,,8/21/2023  9:00:00 AM,SCHEDULE,,14,DAYS,,12/1/2023  9:00:00 AM,EMAIL,,dart@chop.edu,[survey-participant-email],,,,Log back in and keep learning!,"'$(cat pathways/$pathway/nudge_alert.md)'",,{},{},Y,,,N' >>$alert_csv
+echo ',Inactive nudge '$pathway' pathway,LOGIC,,ANY,"[pathway] = ""'$pathway'"" and datediff([survey-date-completed:'$pathway'_pathway][last-instance], ""today"", ""d"", ""ymd"") > 14 AND [stop_emails] <> ""1"" AND [complete] <> ""2""",Y,RECORD,DATE,,,,,,,,8/21/2023  9:00,SCHEDULE,,14,DAYS,,12/1/2023  9:00,EMAIL,,dart@chop.edu,[survey-participant-email],,,,Log back in and keep learning!,"'$(cat pathways/$pathway/nudge_alert.md)'",,{},{},Y,,,N' >>$alert_csv
 
 
